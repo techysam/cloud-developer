ab070f1f888a5caa7cba7607e9069844e4632230
Samuel Benjamin Baffoe <samuelbenjaminbaffoe@gmail.com>
2021-12-06 04:16:02 +0000

    Changed Dynamo and Api ID

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,8 +1,11 @@
-const apiId = 'zmh7v1es8j'
+const apiId = 'jc2tirbdsl'
+//Having issues making it work without my old apiid zmh7v1es8j. It seems it is the reason why a todo cannot
+//be deleted and upload is impossible. Please I have searched all over but cannot solve it.
+//Please help me solve it you can email me at samuelbenjamibaffoe@gmail.com
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-nzq7t-ek.us.auth0.com',           // Auth0 domain
-  clientId: 'rLpsM5Sb5HpdTDuq3M2JpqRPRwy3Rii3',  // Auth0 client id
+  clientId: 'FgaVpSWQ5wHsCZDomZp3mrOwMFXBXLez',  // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
